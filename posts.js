14778a76aeeeb5bb81f16701f85d2e44bcabb378
Tyson Fitzgerald <tfitzgerald779@gmail.com>
2025-08-19 22:25:47 -0400

    removed posts.js data for test blog post

--- posts.js
+++ posts.js
@@ -1,9 +1,5 @@
 const postsData = [
     {
-        slug: 'why-core-web-vitals-matter',
-        title: 'Core Web Vitals: More Than Just Speed',
-        category: 'Web Design',
-        description: 'We explore the three pillars of Core Web Vitals and why they are critical for user experience and rankings.',
-        thumbnail: 'posts/why-core-web-vitals-matter/thumbnail.jpg' 
+        
     }
 ];
