596c2f4993ea0571999a7457721c31fe163689fb
Tyson Fitzgerald <tfitzgerald779@gmail.com>
2025-08-21 01:05:59 -0400

    added blog post 1

--- posts.js
+++ posts.js
@@ -1,3 +1,9 @@
 const postsData = [
-
+    {
+        slug: 'welcome-to-aetheri',
+        title: 'Welcome to Aetheri Web Design!',
+        category: 'First Post',
+        description: 'Intro to Aetheri',
+        thumbnail: 'posts/welcome-to-aetheri/thumbnail.jpg'
+    }
 ];
